08be3b2943d10649e211f9500a6b0166c3f9dfc6
misterdn <misterdn@gmail.com>
2011-02-13 08:18:26 +0800

    Registration. Undone.

---
+++ register.php
@@ -0,0 +1,54 @@
+<?php
+	require_once('inc/header.php');
+	
+?>	
+<html>
+<head>
+<title></title>
+<link href="css/screen.css" type="text/css" rel="stylesheet" media="screen,projection" />
+
+</head>
+<body>
+<div id="layout">
+
+<div id="header">
+
+<h1 id="logo"><a href="index.php" title="#"><span>Project</span> Cognito</a></h1>
+<hr class="noscreen" />   
+
+<?php include('inc/nav.php')  ?>            
+
+<hr class="noscreen" />  
+
+</div>
+
+<div id="main">
+
+<div id="main-box">
+<div id="quote"><br><br></div>
+</div>
+
+<div id="content">
+<h2>Register</h2>
+<p>
+
+
+</p>
+
+
+</div>
+
+<div id="footer">
+<p id="copyright">&copy; 2008 - <a href="/">Your name</a></p>
+
+<!-- Please don't delete this. You can use this template for free and this is the only way that you can say thanks to me -->
+<p id="dont-delete-this">Design by <a href="http://www.davidkohout.cz">David Kohout</a> | Our tip: <a href="http://www.junglegym.cz/uvodni-stranka.aspx" title="Dětská Hřiště Jungle Gym">Dětská Hřiště</a></p>
+<!-- Thank you :) -->
+
+</div>
+
+</div>
+</div>
+
+</body>
+</html>
